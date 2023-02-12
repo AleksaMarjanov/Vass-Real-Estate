863596d8ef8dd17b8f5a8856b2cd2982275d12ec
MrWhite1338 <aleksa.marjanov@outlook.com>
2023-02-12 12:02:55 -0600

    add Listings Image/component

--- lib/sanity.client.ts
+++ lib/sanity.client.ts
@@ -1,6 +1,5 @@
 import sanityClient from "@sanity/client";
-// import imageUrlBuilder from "@sanity/image-url";
-import imageUrlBuilder from "@sanity/image-url";
+
 
 export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
 export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || "production";
@@ -13,6 +12,3 @@ export const client = sanityClient({
     useCdn: true,
 })
 
-const builder = imageUrlBuilder(client);
-
-export const urlFor = (source : any) => builder.image(source);
