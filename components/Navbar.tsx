48afbedd3cab962752c0f349a24ee7d7ba18317d
MrWhite1338 <aleksa.marjanov@outlook.com>
2023-02-12 14:48:47 -0600

    ..

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -16,7 +16,7 @@ const Navbar = () => {
       initial="hidden"
       whileInView="show"
       viewport={{ once: false, amount: 0.25}}
-      className={`sm:px-16 px-3 py-8 bg-secondary-black`}
+      className={`sm:px-16 px-3 py-2 bg-secondary-black`}
       >
       <div className='w-full mx-auto flex items-center justify-between gap-8 py-5 left-0 top-0 '>
       <Link href='/' className='cursor-pointer hover:opacity-60 transition-all ease-in-out duration-700'>
