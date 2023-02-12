9e9633665691d1864fcce42ff3a1580862e58eaf
MrWhite1338 <aleksa.marjanov@outlook.com>
2023-02-11 21:45:23 -0600

    fetching data from sanity socials

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,7 +1,11 @@
 import { previewData } from "next/headers";
+import { groq } from "next-sanity";
+
+const query = groq`
+  *[_type=='social']
+`
 
 const HomePage = () => {
- 
   if(previewData()) {
     return <div>Preview Mode</div>
   }
