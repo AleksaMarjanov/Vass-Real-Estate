8f6118af51c4603f8bcee487918d2128a1f958a6
MrWhite1338 <aleksa.marjanov@outlook.com>
2023-02-12 12:28:39 -0600

    ..

--- app/listings/page.tsx
+++ app/listings/page.tsx
@@ -1,19 +1,37 @@
 import ListingsList from "@/components/ListingsList";
 import { groq } from 'next-sanity'
 import { client } from "@/lib/sanity.client";
+import PreviewSuspense from '../../components/PreviewSuspense'
+import { previewData } from "next/headers";
 
-
-const query = groq`*[_type == 'listing']`;
-
+const query = groq`
+*[_type=='listing'] {
+    ...,
+    author->,
+    categories[]->
+} | order(_createdAt desc)
+`;
 
 const Listings = async () => {
     
+    if(previewData()) {
+      return (
+        <PreviewSuspense fallback={(
+          <div role="status">
+            <p className="text-center text-lg animate-pulse text-white">
+              Loading Preview Data...
+            </p>
+          </div>
+        )}>
+  
+        </PreviewSuspense>
+        )
+    }
     const listings = await client.fetch(query)
-    console.log(listings)
-    
     return(
-        <ListingsList listings={listings} />
-    )
-}
+            <ListingsList listings={listings} />
+    ) 
+  }
+
 
 export default Listings;
--- components/ListingsList.tsx
+++ components/ListingsList.tsx
@@ -9,19 +9,37 @@ type Props = {
 }
 
 const ListingsList = ({ listings } : Props) => {
-  console .log(listings.length)
   
   return (
     <div>
       {listings.map(listing => (
-        <div key={listing._id}>
-            <div>
+        <div key={listing._id} className="group flex flex-col cursor-pointer">
+            <div className="relative max-w-[540px] h-80 drop-shadow-xl group-hover:scale-105 transition-transform duration-200 ease-out object-center object-contain">
               <Image 
               src={urlFor(listing.mainImage).url()}
               alt={listing.author.name}
-              width={500}
-              height={500}
+              fill
               />
+              <div className='absolute bottom-0 w-full bg-opacity-20 bg-black backdrop-blur-lg rounded drop-shadow-lg text-white p-5 flex justify-between'>
+                  <div>
+                    <p className='font-bold'>{listing.title}</p>
+                    <p>{new Date(listing._createdAt).toLocaleDateString("en-US", {
+                      day: "numeric",
+                      month: "long",
+                      year: "numeric",
+                    })}
+                    </p>
+                  </div>
+                  
+                  <div className="flex flex-col md:flex-row gap-y-2 md:gap-x-2 items-center">
+                    {listing.categories.map(category => (
+                      <div key={category._id}>
+                        <p className="text-bold">{category.title}</p>
+                      </div>
+                    ))}
+                  </div>
+                  
+              </div>
             </div>
         </div>
       ))}
