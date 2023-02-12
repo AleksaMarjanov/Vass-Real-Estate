19603a76084d0d832b4cfca6861a874494bed2a8
MrWhite1338 <aleksa.marjanov@outlook.com>
2023-02-11 21:04:50 -0600

    add Layout to each page

---
+++ app/about/layout.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import "../../styles/globals.css";
+import { Montserrat } from "@next/font/google";
+import { Navbar, Footer } from "@/components";
+
+const montserrat = Montserrat({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+  variable: "--font-montserrat",
+});
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      {/*
+        <head /> will contain the components returned by the nearest parent
+        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
+      */}
+      <body className={`${montserrat.className}  bg-secondary-blue text-white`}>
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
