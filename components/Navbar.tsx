6d50047dbe1db23cf5fc327df1b0cbc4c4c15858
MrWhite1338 <aleksa.marjanov@outlook.com>
2023-02-15 14:54:31 -0600

    updated AboutPage & Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -68,14 +68,17 @@ const Navbar = () => {
 
 
         <div
-          className="md:hidden block"
+          className="md:hidden block relative"
           onClick={() => setToggle(true)}
         >
-          <img
+          <div className="w-[30px] h-[30px] relative cursor-pointer">
+          <Image
             src="/menu.svg"
             alt="menu"
-            className="w-[35] h-[35] flex object-contain items-center justify-center cursor-pointer"
+            fill
+            className="object-contain"
           />
+          </div>
         </div>
 
         {toggle && (
@@ -106,13 +109,13 @@ const Navbar = () => {
                 {["listings", "projects", "transactions", "about"].map(
                   (item, index) => (
                     <div key={item + index} >
-                      <li className="m-4"
+                      <li className="m-4 group"
                         key={`link-${item}`}
                       >
                         <a
                           href={`/${item}`}
-                          className="text-white no-underline xl:text-2xl sm:text-xl
-                    uppercase font-semibold transition-all ease-in-out duration-500 hover:text-primary-black flex items-center justify-center relative z-[50]"
+                          className="text-white xl:text-2xl sm:text-xl
+                          uppercase bg-left-bottom bg-gradient-to-r from-[#F7AB0A] to-[#F7AB0A] bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out no-underline font-bold relative z-[50]"
                           onClick={() => setToggle(false)}
                         >
                           {item}
@@ -123,6 +126,19 @@ const Navbar = () => {
                 )}
                 </div>
               </div>
+              <a href="/contact" className="relative inline-block text-lg group mr-8 mt-72">
+            <span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white ">
+              <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
+              <span className="absolute right-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-left rotate-90 translate-x-full translate-y-12 bg-[#F7AB0A]  group-hover:rotate-180 ease"></span>
+              <span className="relative">
+                Contact
+              </span>
+            </span>
+            <span
+              className="absolute bottom-0 right-0 w-full h-12  -mb-1 -mr-1 transition-all duration-200 ease-linear bg-[#F7AB0A]  rounded-lg group-hover:mb-0 group-hover:mr-0"
+              data-rounded="rounded-lg"
+            ></span>
+          </a>
             </ul>
           </motion.div>
         )}
