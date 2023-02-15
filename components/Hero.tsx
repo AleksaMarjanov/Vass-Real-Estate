a2a01a1e5a0b1efbb9deba265b59ac88f33b19fd
MrWhite1338 <aleksa.marjanov@outlook.com>
2023-02-15 17:03:19 -0600

    motion div added to hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -17,12 +17,12 @@ className={`${styles.xPaddings} flex items-center justify-center flex-row max-[7
     <motion.div
     variants={slideIn('left','tween', 0.3, 0.85)}
     >
-      <section className="flex flex-col items-left justify-left mt-0 max-[425px]:px-2">
         <motion.div
         variants={staggerContainer}
         initial="hidden"
         whileInView="show"
         viewport={{ once: false, amount: 0.25}}
+        className="flex flex-col items-left justify-left mt-0 max-[425px]:px-2"
         >
         <motion.h1 variants={textVariant(1.1)}  className="text-5xl max-[768px]:text-3xl text-bold py-12 max-[425px]:py-2">Discover Your Perfect <span className="underline decoration-[#F7AB0A] text-extrabold">Property</span></motion.h1>
         <motion.div
@@ -34,7 +34,9 @@ className={`${styles.xPaddings} flex items-center justify-center flex-row max-[7
         </motion.div>
         </motion.div>
 
-        <div className="max-[425px]:flex items-center justify-center">
+        <motion.div 
+        variants={textVariant(1.6)}
+        className="max-[425px]:flex items-center justify-center">
         <a href="/listings" className="relative inline-block text-lg group mb-10 mt-10">
             <span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white ">
               <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
@@ -48,8 +50,7 @@ className={`${styles.xPaddings} flex items-center justify-center flex-row max-[7
               data-rounded="rounded-lg"
             ></span>
           </a>
-        </div>
-      </section>   
+        </motion.div>
       </motion.div>
       
         <motion.div 
