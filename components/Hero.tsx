659d9d9cd6ac79f2ec9d3fc0ba607138fe3570a8
MrWhite1338 <aleksa.marjanov@outlook.com>
2023-02-15 16:59:58 -0600

    ..

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,21 +3,38 @@
 import Image from 'next/image'
 import styles from "@/styles";
 import { motion } from "framer-motion";
-import { fadeIn, slideIn, staggerContainer } from "@/utils/motion";
+import { fadeIn, slideIn, staggerContainer, textVariant } from "@/utils/motion";
 
 const Hero = () => {
     return(
-<div className={`${styles.xPaddings} flex items-center justify-center flex-row max-[768px]:flex-col`}
->
+<motion.div
+variants={staggerContainer}
+initial="hidden"
+whileInView="show"
+viewport={{ once: true }}
+className={`${styles.xPaddings} flex items-center justify-center flex-row max-[768px]:flex-col`}
+>      
+    <motion.div
+    variants={slideIn('left','tween', 0.3, 0.85)}
+    >
       <section className="flex flex-col items-left justify-left mt-0 max-[425px]:px-2">
-        <div>
-        <h1 className="text-5xl max-[768px]:text-3xl text-bold py-12 max-[425px]:py-2">Discover Your Perfect <span className="underline decoration-[#F7AB0A] text-extrabold">Property</span></h1>
+        <motion.div
+        variants={staggerContainer}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: false, amount: 0.25}}
+        >
+        <motion.h1 variants={textVariant(1.1)}  className="text-5xl max-[768px]:text-3xl text-bold py-12 max-[425px]:py-2">Discover Your Perfect <span className="underline decoration-[#F7AB0A] text-extrabold">Property</span></motion.h1>
+        <motion.div
+        variants={textVariant(1.2)}
+        >
         <p className="text-normal text-md text-[#d3d2d2]">
         Your Trusted Real Estate Broker for Exceptional Service and Results
         </p>
-        </div>
-        <div className="max-[425px]:flex items-center justify-center">
+        </motion.div>
+        </motion.div>
 
+        <div className="max-[425px]:flex items-center justify-center">
         <a href="/listings" className="relative inline-block text-lg group mb-10 mt-10">
             <span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white ">
               <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
@@ -32,19 +49,21 @@ const Hero = () => {
             ></span>
           </a>
         </div>
-        
       </section>   
+      </motion.div>
       
-        <div className="w-[1024px] h-[480px] max-[768px]:w-[480px] max-[768px]:h-[260px] max-[425px]:w-[280px] relative right-0">
+        <motion.div 
+        variants={slideIn('right','tween', 0.3, 0.85)}
+        className="w-[1024px] h-[480px] max-[768px]:w-[480px] max-[768px]:h-[260px] max-[425px]:w-[280px] relative right-0">
       <Image 
       src="/bestPlace.svg"
       alt="heroImage"
       fill
     //   className="object-contain"
       />
-        </div>
+        </motion.div>
 
- </div> 
+ </motion.div> 
     )
 }
 
