0e114760253a4aac788a2ba7f15cbad010dc9c8c
MrWhite1338 <aleksa.marjanov@outlook.com>
2023-02-14 18:53:18 -0600

    add Transitions and go back butto0n

--- app/(user)/listing/[slug]/page.tsx
+++ app/(user)/listing/[slug]/page.tsx
@@ -47,6 +47,32 @@ async function Listing({ params: { slug } }: Props) {
     
     return (
       <article className="px-10 pb-28 mt-5">
+             <a href="/listings" className="relative inline-block text-lg group mb-10 mt-10">
+        <span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white ">
+          <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50 "></span>
+          <span className="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-[#F7AB0A] group-hover:-rotate-180 ease"></span>
+          <span className="relative">
+            <svg
+              aria-hidden="true"
+              className="w-5 h-5 mr-2 inline-flex"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M7.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l2.293 2.293a1 1 0 010 1.414z"
+                clip-rule="evenodd"
+              />
+            </svg>
+            Go back
+          </span>
+        </span>
+        <span
+          className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-[#F7AB0A] rounded-lg group-hover:mb-0 group-hover:mr-0"
+          data-rounded="rounded-lg"
+        ></span>
+      </a>
         <section className="space-y-2 border-[#F7AB0A] text-white">
           <div className="relative min-h-56 flex flex-col md:flex-row justify-between">
             <div className="absolute top-0 w-full h-full opacity-30 blur-sm p-10">
--- components/ListingsList.tsx
+++ components/ListingsList.tsx
@@ -27,7 +27,11 @@ const ListingsList = ({ listings } : Props) => {
       <motion.div 
       variants={slideIn('left', 'tween', 0.3, 1 )}
       className='text-bold text-2xl flex items-left justify-left px-12 py-6'>Available listings</motion.div>
+      <motion.div
+      variants={slideIn('left','spring', 0.5, 1.2 )}
+      >
       <hr className="border-[#F7AB0A] mb-10" />
+      </motion.div>
 
     <div className="grid grid-cols-1 md:grid-cols-3 px-10 gap-10 gap-y-16 pb-24">
       {listings.map(listing => (
@@ -36,11 +40,11 @@ const ListingsList = ({ listings } : Props) => {
         variants={fadeIn("up", "tween", 0.5, 1)}
         initial="hidden"
         whileInView="show"
-        viewport={{ once: false, amount: 0.25}}
+        viewport={{ once: true }}
          className="group flex flex-col cursor-pointer">
             <div className="relative w-full h-80 drop-shadow-xl group-hover:scale-105 transition-transform duration-200 ease-out">
               <Image 
-              className="object-cover object-left lg:object-center"
+              className="object-cover object-left lg:object-center rounded-lg"
               src={urlFor(listing.mainImage).url()}
               alt={listing.author.name}
              fill 
