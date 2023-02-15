bfb62cabcb617f3997fe14ef906d408b212b8fff
MrWhite1338 <aleksa.marjanov@outlook.com>
2023-02-15 16:49:41 -0600

    ..Add Correct heigth and width for bigger call to action button

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -21,7 +21,7 @@ const Hero = () => {
         <a href="/listings" className="relative inline-block text-lg group mb-10 mt-10">
             <span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white ">
               <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
-              <span className="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-[#F7AB0A] group-hover:-rotate-180 ease"></span>
+              <span className="absolute left-0 w-64 h-64 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-[#F7AB0A] group-hover:-rotate-180 ease"></span>
               <span className="relative">
                 Browse Properties
               </span>
