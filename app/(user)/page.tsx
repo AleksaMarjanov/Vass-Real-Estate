e3cdc1df345204ada51a74e80fdb51c69e66a57f
MrWhite1338 <aleksa.marjanov@outlook.com>
2023-02-16 17:25:27 -0600

    ..

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,9 +1,14 @@
+"use client";
+
 import { previewData } from "next/headers";
 import { groq } from "next-sanity";
 import PreviewSuspense from '../../components/PreviewSuspense'
 import Image from "next/image";
 import styles from "@/styles";
+import Link from "next/link";
+import { motion } from "framer-motion";
 import { Hero, BulletSection } from '@/components'
+import { fadeIn, staggerContainer } from "@/utils/motion";
 
 
 const HomePage = () => {
@@ -23,10 +28,28 @@ const HomePage = () => {
 
   return(
     <main className="items-center justify-center py-12">
-      <section>
+      <section id="hero">
       <Hero />
+      <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25}}
+      >
+
+      <a href="#bulletsection" rel="norefferer" className="flex items-center justify-center mt-[64px] cursor-pointer">
+      <motion.img
+          variants={fadeIn('up', 'tween', 0.3,1)}
+          src="/arrow-down.svg"
+          alt="arrow down"
+          className="w-[38px] h-[38px] drop-shadow-xl hover:scale-105 transition-transform duration-200 ease-out object-contain  flex items-center justify-center rounded-full bg-[#F7AB0A] px-1 py-1"
+          >
+          </motion.img>
+      </a>
+
+      </motion.div>
       </section>
-      <section>
+      <section id="bulletsection">
         <BulletSection />
       </section>      
     </main>
--- components/BulletSection.tsx
+++ components/BulletSection.tsx
@@ -6,19 +6,19 @@ import styles from '@/styles'
 
 const BulletSection = () => {
   return (
-        <section id="bulletsection" className={`${styles.xPaddings} w-full flex flex-row space-x-6 px-6 mt-24
+        <section className={`${styles.xPaddings} w-full flex flex-row space-x-6 px-6 mt-24 max-[768px]:gap-12
         max-[768px]:flex-col items-center justify-center bg-[#0086bb]/20 h-[300px] text-white`}>
             <div className="flex items-center justify-center flex-col">
                 <h2>Client-Centered Approach</h2>
-                <p className="text-[12px]">Deano Vass Real Estate takes a client-centered approach to every transaction, prioritizing the needs and preferences of each individual client.</p>
+                <p className="text-[12px]">Deano Vass Real Estate prioritizes each client&apos;s needs and preferences in every transaction.</p>
             </div>
             <div className="flex items-center justify-center flex-col">
                 <h2>Trusted Expertise</h2>
-                <p className="text-[12px]">With years of experience and a comprehensive understanding of the local market, Deano Vass Real Estate is a trusted expert in the real estate industry.</p>
+                <p className="text-[12px]">Deano Vass Real Estate is a trusted expert in the local real estate industry.</p>
             </div>
             <div className="flex items-center justify-center flex-col">
                 <h2>Result-Driven Strategies</h2>
-                <p className="text-[12px]">Our team of professionals utilizes results-driven strategies to help clients achieve their real estate goals, with a focus on maximizing value and minimizing risk.</p>
+                <p className="text-[12px]">We use results-driven strategies to help clients achieve their real estate goals while minimizing risk.</p>
             </div>
         </section>
     )
