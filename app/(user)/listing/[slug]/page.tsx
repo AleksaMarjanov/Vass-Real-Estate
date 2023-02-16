6f9be97ea53f28b9578fe0b6315f35e90feb6c24
MrWhite1338 <aleksa.marjanov@outlook.com>
2023-02-16 16:59:20 -0600

    Completed Hero Section..

--- app/(user)/listing/[slug]/page.tsx
+++ app/(user)/listing/[slug]/page.tsx
@@ -51,7 +51,7 @@ async function Listing({ params: { slug } }: Props) {
              <a href="/listings" className="relative inline-block text-lg group mb-10 mt-10">
         <span className="relative z-10 block px-5 py-3 max-[425px]:px-3 max-[425px]:py-1 overflow-hidden font-medium max-[425px]:font-normal leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white ">
           <span className="absolute inset-0 w-full h-full px-5 py-3  max-[425px]:px-3 max-[425px]:py-2 rounded-lg bg-gray-50 "></span>
-          <span className="absolute left-0 w-48 h-48 max-[425px]:w-36 max-[425px]:w-36 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-[#F7AB0A] group-hover:-rotate-180 ease"></span>
+          <span className="absolute left-0 w-48 h-48 max-[425px]:w-36 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-[#F7AB0A] group-hover:-rotate-180 ease"></span>
           <span className="relative">
             <svg
               aria-hidden="true"
