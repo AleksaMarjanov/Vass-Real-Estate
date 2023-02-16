85dc22c901a1ae7fbe085e9aa9bcd31425d8ad02
MrWhite1338 <aleksa.marjanov@outlook.com>
2023-02-15 20:13:31 -0600

    skew-y-12 bg

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -12,7 +12,7 @@ variants={staggerContainer}
 initial="hidden"
 whileInView="show"
 viewport={{ once: true }}
-className={`${styles.xPaddings} flex items-center justify-center flex-row max-[768px]:flex-col`}
+className={`${styles.xPaddings} items-center flex justify-center flex-row max-[768px]:flex-col relative z-0`}
 >      
     <motion.div
     variants={slideIn('left','tween', 0.3, 0.85)}
@@ -22,7 +22,7 @@ className={`${styles.xPaddings} flex items-center justify-center flex-row max-[7
         initial="hidden"
         whileInView="show"
         viewport={{ once: false, amount: 0.25}}
-        className="flex flex-col items-left justify-left mt-0 max-[425px]:px-2"
+        className="flex flex-col relative z-[50] items-left justify-left mt-0 max-[425px]:px-2"
         >
         <motion.h1 variants={textVariant(1.1)}  className="text-5xl max-[768px]:text-3xl text-bold py-12 max-[425px]:py-2">Discover Your Perfect <span className="underline decoration-[#F7AB0A] text-extrabold">Property</span></motion.h1>
         <motion.div
@@ -36,7 +36,7 @@ className={`${styles.xPaddings} flex items-center justify-center flex-row max-[7
 
         <motion.div 
         variants={textVariant(1.6)}
-        className="max-[425px]:flex items-center justify-center">
+        className="max-[768px]:flex items-center justify-center">
         <a href="/listings" className="relative inline-block text-lg group mb-10 mt-10">
             <span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white ">
               <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
@@ -52,10 +52,12 @@ className={`${styles.xPaddings} flex items-center justify-center flex-row max-[7
           </a>
         </motion.div>
       </motion.div>
+
+      
       
         <motion.div 
         variants={slideIn('right','tween', 1, 1)}
-        className="w-[1024px] h-[480px] max-[768px]:w-[480px] max-[768px]:h-[260px] max-[425px]:w-[280px] relative right-0">
+        className="w-[1024px] h-[480px] max-[768px]:w-[480px] max-[768px]:h-[260px] max-[425px]:w-[280px] relative z-[10] right-0">
       <Image 
       src="/bestPlace.svg"
       alt="heroImage"
@@ -63,7 +65,18 @@ className={`${styles.xPaddings} flex items-center justify-center flex-row max-[7
     //   className="object-contain"
       />
         </motion.div>
-
+        <motion.div
+        variants={staggerContainer}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: false, amount: 0.25}}
+        >
+        <motion.div
+        variants={fadeIn('left', 'tween', 0.8, 0.85)}
+        >
+            <div className="w-full absolute top-[30%] bg-[#0086bb]/10 left-0 h-[500px] max-[768px]:h-[300px] -skew-y-12" />
+        </motion.div>
+        </motion.div>
  </motion.div> 
     )
 }
