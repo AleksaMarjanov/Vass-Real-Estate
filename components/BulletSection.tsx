f70674bda7e116bdb68ca3155added31992300fe
MrWhite1338 <aleksa.marjanov@outlook.com>
2023-02-16 20:02:45 -0600

    ..

--- components/BulletSection.tsx
+++ components/BulletSection.tsx
@@ -3,22 +3,32 @@
 import React from 'react'
 import { motion } from 'framer-motion'
 import styles from '@/styles'
+import Image from 'next/image';
 
 const BulletSection = () => {
   return (
-        <section className={`${styles.xPaddings} w-full flex flex-row space-x-6 px-6 mt-24 max-[768px]:gap-12
+        <section className={`w-full px-36 flex flex-row space-x-6 mt-24 max-[768px]:gap-12
         max-[768px]:flex-col items-center justify-center h-[300px] text-white `}>
-            <div className="flex items-center justify-center flex-col">
+            <div className="flex items-left justify-left flex-col space-y-4">
+                <div className="flex flex-row items-left justify-left gap-3 ">
+                <Image src='/clientApproach.svg' alt="clientApproach" width={30} height={30} priority />
                 <h2>Client-Centered Approach</h2>
-                <p className="text-[12px]">Deano Vass Real Estate prioritizes each client&apos;s needs and preferences in every transaction.</p>
+                </div>
+                <p className="text-[12px] flex items-left justify-center">Deano Vass Real Estate prioritizes each client&apos;s needs and preferences in every transaction.</p>
             </div>
-            <div className="flex items-center justify-center flex-col">
+            <div className="flex items-left justify-left flex-col space-y-4">
+            <div className="flex flex-row items-left justify-left gap-3">
+                <Image src='/Asset 1.png' alt="" width={30} height={30} priority />
                 <h2>Trusted Expertise</h2>
-                <p className="text-[12px]">Deano Vass Real Estate is a trusted expert in the local real estate industry.</p>
             </div>
-            <div className="flex items-center justify-center flex-col">
+                <p className="text-[12px] flex items-left justify-left">Deano Vass Real Estate is a trusted expert in the local real estate industry.</p>
+            </div>
+            <div className="flex items-left justify-left flex-col space-y-4">
+            <div className="flex flex-row items-left justify-left gap-3">
+                <Image src='/clientApproach.svg' alt="clientApproach" width={30} height={30} priority />
                 <h2>Result-Driven Strategies</h2>
-                <p className="text-[12px]">We use results-driven strategies to help clients achieve their real estate goals while minimizing risk.</p>
+            </div>
+                <p className="text-[12px] flex items-left justify-center">We use results-driven strategies to help clients achieve their real estate goals while minimizing risk.</p>
             </div>
         </section>
     )
