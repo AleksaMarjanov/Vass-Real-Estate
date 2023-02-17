d2c22fbf52bdfde4e510ec5b955e7d2d9579c10e
MrWhite1338 <aleksa.marjanov@outlook.com>
2023-02-17 17:38:10 -0600

    add useEffect to fix bug "remove extra attributes at SSC Component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 
 import styles from "@/styles";
-import { Hero, BulletSection } from '@/components'
+import { Hero, BulletSection, Services } from '@/components'
 
 
 const HomePage = () => {
@@ -15,6 +15,9 @@ const HomePage = () => {
         {/* Work around TypeScript Error Type 'Promise<Element>' is missing the following properties from type 'ReactElement<any, any>': type, props, key `ts(2786)`*/}
         {/* @ts-expect-error Server Component */}
         <BulletSection/>
+      </section>
+      <section id="services">
+        <Services />
       </section>      
     </main>
   ) 
---
+++ components/FooterList.tsx
@@ -0,0 +1,74 @@
+'use client';
+
+import {useState, useEffect } from 'react'
+import { motion } from 'framer-motion'
+import { footerVariants } from "../utils/motion";
+import styles from '@/styles';
+import Image from 'next/image';
+import { groq } from 'next-sanity';
+import { client } from '@/lib/sanity.client';
+import { Social } from '@/typings';
+import { SocialIcon } from 'react-social-icons';
+
+
+type Props = {
+  socials: Social[]
+}
+
+const Footer = ({ socials } : Props) => {
+
+  
+  return (
+    <motion.footer
+    variants={footerVariants}
+    initial="hidden"
+  whileInView="show"
+  className={`${styles.xPaddings} py-8`}
+    >
+          <div className="mb-[50px] h-[2px] bg-white opacity-10" />
+
+        <div className="flex flex-col">
+
+          <div className="flex items-center justify-between flex-wrap gap-4">
+            <div className='flex items-center justify-center'>
+          <Image
+            src='/logoSvg.svg'
+            alt="Vass Real Estate Logo"
+            width={500}
+            height={500}
+            priority
+            />
+            </div>
+            <p className="font-normal text-[14px] text-white opacity-50">
+            Copyright © {new Date().getFullYear()} Vass Real Estate Solutions. All Rights Reserved
+            </p>
+          
+             <div className="flex gap-4">
+            {socials.map((social) => (
+        <SocialIcon
+          key={social._id}
+          url={social.url}
+          fgColor="white"
+          bgColor="transparent"
+        />
+        ))} 
+            </div>  
+          </div>
+        </div>
+    </motion.footer>
+  )
+}
+
+export default Footer
+
+// export const getStaticProps: GetStaticProps<Props> = async () => {
+//   const socials: Social[] = await fetchSocial();
+
+
+//   return {
+//     props: {
+//       socials
+//     },
+    
+//   }
+// }  
