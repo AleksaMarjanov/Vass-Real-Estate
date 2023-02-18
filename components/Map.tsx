ce6f3ba373939694c3b40c2be39d77be8a17be06
MrWhite1338 <aleksa.marjanov@outlook.com>
2023-02-18 13:37:22 -0600

    ..

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,8 +1,5 @@
-import { groq } from 'next-sanity'
 import React from 'react'
-import MapParams from './MapParams';
 import dynamic from "next/dynamic";
-import { client } from '@/lib/sanity.client';
 import { Listing } from '@/typings';
 
 const MapWithNOSSR = dynamic(() => import("./MapParams"), {
@@ -17,7 +14,7 @@ type Props = {
 const Map = async ({ listing } : Props) => {
 
 return (
-    <div className="relative z-[0] h-[60vh] w-[40%] overflow-hidden no-repeat">
+    <div className="relative z-[0] h-[60vh] lg:w-[40%] w-[80%] max-[425px]:w-full max-[425px]:px-2 overflow-hidden no-repeat">
       <MapWithNOSSR listing={listing}/>
     </div>
   )
