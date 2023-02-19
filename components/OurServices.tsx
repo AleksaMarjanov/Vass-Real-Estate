9d835f291f7b5454f11d3bb4885677e8e83d1fe3
Aleksa Marjanov <Aleksa.marjanov@outlook.com>
2023-02-19 21:04:43 +0000

    servicecard

--- components/OurServices.tsx
+++ components/OurServices.tsx
@@ -3,6 +3,7 @@
 import styles from '@/styles';
 import { slideIn, staggerContainer } from '@/utils/motion';
 import { motion } from 'framer-motion';
+import Image from 'next/image';
 
 const OurServices = () => {
   return (
@@ -16,6 +17,17 @@ const OurServices = () => {
     <motion.div variants={slideIn('up', 'tween', 0.3, 0.85)}>
             <h3 className="text-5xl">OurServices</h3>
     </motion.div>
+
+      <motion.div
+      variants={slideIn('right', 'tween', 0.35, 0.85)}
+      > 
+          <Image src="/apartment.svg"
+        alt="Services"
+        width={500}
+          height={500}
+          priority
+          />
+      </motion.div>
     </motion.div>
 </section>
   )
