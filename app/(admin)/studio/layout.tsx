7660172d9f4a8e7826c419cf7424098e108b0620
MrWhite1338 <aleksa.marjanov@outlook.com>
2023-02-20 18:58:42 -0600

    ..

--- app/(admin)/studio/layout.tsx
+++ app/(admin)/studio/layout.tsx
@@ -1,10 +1,21 @@
 'use client';
 
+import { useEffect, useState } from 'react';
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+
+  const [isSSR, setIsSSR] = useState(true)
+
+  useEffect(() => {
+    setIsSSR(false)
+  }, [])
+
+  if (isSSR) return null;
+
   return (
     <html>
       <body>{children}</body>
