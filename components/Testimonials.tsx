11d7502cdf033803ce207609d29adb9e188c95a3
AleksaMarjanov <aleksa.marjanov@outlook.com>
2023-02-22 14:22:09 -0600

    ..

---
+++ components/Testimonials.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import React from 'react'
+import { Pagination } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+const Testimonials = () => {
+  return (
+    <div className="relative flex items-center justify-center">
+            <Swiper
+            
+            >
+                <SwiperSlide>
+
+                </SwiperSlide>
+            </Swiper>
+    </div>
+  )
+}
+
+export default Testimonials
