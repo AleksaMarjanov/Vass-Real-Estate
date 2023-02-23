92d369ba9db40c377616584ba16d50cb27abf963
AleksaMarjanov <aleksa.marjanov@outlook.com>
2023-02-23 06:27:47 -0600

    trying to add test section again

--- components/Testimonials.tsx
+++ components/Testimonials.tsx
@@ -1,11 +1,26 @@
 "use client";
 
+
+
 import React from 'react'
-import { Pagination } from 'swiper';
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Autoplay } from "swiper";
+import "swiper/css";
+import "swiper/css/autoplay";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+import { SliderData } from './../lib/sliderData';
+import { motion } from 'framer-motion';
+import { fadeIn } from '@/utils/motion';
+import Image from 'next/image';
+import { HiChevronLeft, HiChevronRight } from 'react-icons/hi';
+
+
+
 const Testimonials = () => {
   return (
-    <div className="relative flex items-center justify-center">
+    <div className="items-center justify-center">
             <Swiper
             
             >
