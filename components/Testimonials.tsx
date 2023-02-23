f3d895ec336d675d70475fd20ef3d3659c2bfc7a
AleksaMarjanov <aleksa.marjanov@outlook.com>
2023-02-23 06:54:32 -0600

    adding swiper again

--- components/Testimonials.tsx
+++ components/Testimonials.tsx
@@ -33,7 +33,56 @@ const Testimonials = () => {
               priority
               />
              </div>
-            </div>
+        </div>
+        <Swiper
+              modules={[Navigation, Pagination, Autoplay]}
+              slidesPerView={1}
+              navigation={{
+                nextEl: ".image-swiper-button-next",
+                prevEl: ".image-swiper-button-prev",
+                disabledClass: "swiper-button-disabled",
+              }}
+              loop={true}
+              autoplay={{ delay: 5000 }}
+            >
+                {SliderData.map((slide : any, index: any) => (
+                <div className="swiper-slide" key={slide.id + index}>
+                <SwiperSlide key={slide.id + index}>
+                  <div className="relative w-full h-[300px] flex items-start justify-start">
+                    <motion.div
+                    variants={fadeIn("left", "tween", 0.2, 1)}
+                        initial="hidden"
+                        whileInView="show"
+                        className="absolute max-w-[470px] max-h-[600px] p-4 object-contain"
+                    >
+    
+                    <div className="lg:flex lg:flex-col items-start lg:py-4 justify-center top-0">
+                          <span className="text-gray-200 max-[425px]:text-sm text-lg">
+                          <Image src='/quotes.svg' alt="quotes" width={30} height={30} priority />{slide.desc}
+                          </span>
+                        <h1 className="text-xl text-bold">
+                          {slide.title}
+                        </h1>
+                        <div>
+                        </div>
+                    </div>
+                    </motion.div>
+                    </div>
+                      <div className="absolute z-[30] bottom-0 right-0 text-darker-white/70 select-none gap-1  flex-row items-start justify-start mt-32 p-0 lg:flex">
+                      <HiChevronLeft
+                      size={20}
+                        className="z-[2] image-swiper-button-prev cursor-pointer w-[40px] h-[40px] bg-nice-blue hover:bg-darker-white hover:text-nice-blue transition-all ease-in-out duration-500"
+                      />
+                      <HiChevronRight
+                        size={5}
+                        className="z-[2] image-swiper-button-next cursor-pointer w-[40px] h-[40px] bg-nice-blue  hover:bg-darker-white hover:text-nice-blue transition-all ease-in-out duration-500"
+                      />
+                      </div>
+                </SwiperSlide>
+              </div>
+              
+            ))}
+            </Swiper>
       </div>
     </div>
   )
