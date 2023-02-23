bf073e77e585ad6042e4f6f659220abb194d1a14
MrWhite1338 <aleksa.marjanov@outlook.com>
2023-02-23 15:40:52 -0600

    ..

--- components/Testimonials.tsx
+++ components/Testimonials.tsx
@@ -22,8 +22,8 @@ const Testimonials = () => {
   return (
     <div className="relative w-full flex flex-col items-center justify-center min-h-[70vh] mt-12 bg-[#0086bb]/10">
       <h2 className="text-white md:text-3xl text-xl py-12">What people say about us</h2>
-      <div className="flex flex-row items-center justify-center w-full">
-        <div className="hidden md:flex w-1/2 flex-[0.5] items-end justify-end">
+      <div className="flex flex-row items-center justify-center md:items-start md:justify-center w-full">
+        <div className="hidden md:flex items-end justify-end">
           <div className='relative w-[300px] h-[300px] max-[768px]:w-[250px] max-[768px]:h-[250px] flex items-center justify-center object-center'>
             <Image
               src='./testimonials1.svg'
