474a11a1f18f2079058ed66b6a297d58ba64c715
MrWhite1338 <aleksa.marjanov@outlook.com>
2023-02-23 19:17:51 -0600

    ..added metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { Montserrat } from "@next/font/google";
 import { Navbar, Footer } from "@/components";
 import { usePathname } from "next/navigation";
 import { useEffect } from "react";
+import { Metadata } from "next";
 
 const montserrat = Montserrat({
   weight: ["400", "700"],
