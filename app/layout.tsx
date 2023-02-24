5da8a7880d4778cedcdc5be3583ef089c0c67d56
MrWhite1338 <aleksa.marjanov@outlook.com>
2023-02-23 19:45:49 -0600

    trying to put metadata in

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,14 +5,16 @@ import "../styles/globals.css";
 import { Montserrat } from "@next/font/google";
 import { Navbar, Footer } from "@/components";
 import { usePathname } from "next/navigation";
-import { useEffect } from "react";
-import type { Metadata } from "next";
+import { Metadata } from 'next';
 
-export const metadata: Metadata = {
+
+export const metadata = {
   title: 'Vass Real Estate Solutions - Real Estate, Property Managment',
   description: 'Deano Vass Real Estate is a full-service broker company based in Williston, North Dakota, that specializes in providing exceptional residential and commercial real estate services, property management, market analysis, and investment consulting. With a commitment to exceptional client service, local expertise, and innovative use of technology, Deano Vass Real Estate is a trusted expert in the local real estate industry. For more information about their services, please contact them at (701)572-1111',
   keywords: ['Real estate','Broker','Property management','Market analysis','Investment consulting','Residential,Commercial','Williston','North Dakota','Client-centered','Local expertise','Innovative technology','Trusted expert','Exceptional service','Negotiation','Property value','Commission','Timeline','Listings','Buyers','Sellers','Property search','Comparative analysis','Offers','Closing process','Marketing','Promotion','Advocacy','Resources']
 }
+
+
 
 const montserrat = Montserrat({
   weight: ["400", "700"],
@@ -38,23 +40,23 @@ export default function RootLayout({
   // }, [pathname]);
 
 {/* Solution 2.  Fixing error of not scrolling to the top of the page, [slug]/page.tsx top div has id of scroll-top */}
-  const pathname = usePathname()
+  // const pathname = usePathname()
     
-  useEffect(() => {
-      document.scrollingElement?.scroll(0, 0)
-  }, [pathname])
+  // useEffect(() => {
+  //     document.scrollingElement?.scroll(0, 0)
+  // }, [pathname])
   
-  useEffect(() => {
-      setTimeout(() => {
-          document.scrollingElement?.scrollTo(0, 0)
-      }, 50)
-      setTimeout(() => {
-          document.scrollingElement?.scrollTo(0, 0)
-      }, 100)
-      setTimeout(() => {
-          document.scrollingElement?.scrollTo(0, 0)
-      }, 200)
-  }, [])
+  // useEffect(() => {
+  //     setTimeout(() => {
+  //         document.scrollingElement?.scrollTo(0, 0)
+  //     }, 50)
+  //     setTimeout(() => {
+  //         document.scrollingElement?.scrollTo(0, 0)
+  //     }, 100)
+  //     setTimeout(() => {
+  //         document.scrollingElement?.scrollTo(0, 0)
+  //     }, 200)
+  // }, [])
 
   return (
 
