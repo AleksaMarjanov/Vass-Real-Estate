d20d2ae2e66e3c88219be05b97b91e2c996c5f9f
MrWhite1338 <aleksa.marjanov@outlook.com>
2023-02-25 07:26:16 -0600

    ..FRAMER MOTION from left-right causes overflow-y

--- components/faq.tsx
+++ components/faq.tsx
@@ -23,7 +23,7 @@ const FAQ = () => {
       >
       
                 <motion.div
-                variants={fadeIn('left', 'tween', 0.45, 0.85)}
+                variants={fadeIn('up', 'tween', 0.45, 0.85)}
                 className="w-full md:px-24 px-6 min-h-[30vh] md:mt-24 max-[425x]:mt-12">
                   <h1 className="flex items-center justify-center text-2xl max-[425px]:px-6 py-12 max-[425px]:py-6">Frequently Asked</h1>
                   
