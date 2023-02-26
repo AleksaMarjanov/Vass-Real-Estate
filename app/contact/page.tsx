330f0f643727d57c00174ee85ce7e3ea798df1f1
MrWhite1338 <aleksa.marjanov@outlook.com>
2023-02-26 13:09:54 -0600

    Improved performance to 97%, SEO..rest to 100%

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -10,12 +10,12 @@ const MapWithNOSSR = dynamic(() => import("../../components/MapForContact"), {
 const Contact = () => {
     return(
         <div className="flex flex-col items-center justify-center mx-auto h-screen">
-            {/* <div className="w-full">
+            <div className="w-full">
             <Form />
             </div>
             <div className="relative mt-2 z-[0]  max-[425px]:h-[40vh] lg:w-[40%] h-[45vh] md:w-[60%] max-[425px]:w-full max-[425px]:px-2 no-repeat">
                 <MapWithNOSSR />
-             </div> */}
+             </div>
         </div>
     )
 }
