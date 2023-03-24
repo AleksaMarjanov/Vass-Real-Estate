4e2fea8e217221ed6a930acade1d8f5d5170382c
AleksaMarjanov <aleksa.marjanov@outlook.com>
2023-03-24 18:15:55 -0500

    update hero section to more like on a bilboard/make logo on nav bigger

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,104 +7,103 @@ import { fadeIn, slideIn, staggerContainer, textVariant } from "@/utils/motion";
 
 
 const Hero = () => {
-  return (
-    <>
-    <motion.div
-    variants={staggerContainer}
-    initial="hidden"
-    whileInView="show"
-    viewport={{ once: true }}
-    className={` min-h-[60vh] w-full relative`}
-    >
-           <motion.div
-      variants={slideIn('left', 'tween', 0.3, 0.85)}
-      className="w-full relative"
-      >
-          <div className="w-full absolute z-0 -top-[50px] max-[768px]:top-[30px] max-[1440px]:top-[10px] 
+    return (
+        <>
+            <motion.div
+                variants={staggerContainer}
+                initial="hidden"
+                whileInView="show"
+                viewport={{ once: true }}
+                className={` min-h-[60vh] w-full relative`}
+            >
+                <motion.div
+                    variants={slideIn('left', 'tween', 0.3, 0.85)}
+                    className="w-full relative"
+                >
+                    <div className="w-full absolute z-0 -top-[50px] max-[768px]:top-[30px] max-[1440px]:top-[10px] 
           min-[1440px]:top-[10px] bg-[#0086bb]/20 h-[600px] max-[768px]:h-[600px] md:h-[650px] lg:h-[500px] xl:h-[700px] max-[768px]:rounded-br-[160px] rounded-br-[360px]" />
 
-       </motion.div>  
-      <motion.div
-        className={`${styles.xPaddings} items-center flex justify-center flex-col lg:flex-row relative z-[30]`}
-      >
-       
-        <motion.div variants={fadeIn("left", "tween", 0.2, 0.5)}>
-          <motion.div
-            className="flex flex-col relative z-10 items-left justify-left max-[425px]:mt-12 mt-0 max-[425px]:px-2"
-          >
-            
-            <motion.h1
-              variants={textVariant(1.1)}
-              className="text-5xl max-[768px]:text-3xl text-bold py-12 max-[425px]:py-2"
-            >
-              Discover Your Perfect{" "}
-              <span className="underline decoration-[#F7AB0A] text-extrabold">
-                Property
-              </span>
-            </motion.h1>
-            <motion.div variants={textVariant(1.2)}>
-              <p className="text-normal text-md text-[#d3d2d2]">
-                Your Trusted Real Estate Broker for Exceptional Service and
-                Results
-              </p>
+                </motion.div>
+                <motion.div
+                    className={`${styles.xPaddings} items-center flex justify-center flex-col lg:flex-row relative z-[30]`}
+                >
+
+                    <motion.div variants={fadeIn("left", "tween", 0.2, 0.5)}>
+                        <motion.div
+                            className="flex flex-col relative z-10 items-left justify-left max-[425px]:mt-12 mt-0 max-[425px]:px-2"
+                        >
+
+                            <motion.h1
+                                variants={textVariant(1.1)}
+                                className="md:text-5xl text-3xl text-bold py-12 max-[425px]:py-2"
+                            >
+                                Need a{" "}
+                                <span className="underline decoration-[#F7AB0A] text-extrabold">
+                                    Shop?
+                                </span>
+                            </motion.h1>
+                            <motion.div variants={textVariant(1.2)}>
+                                <p className="font-normal text-lg md:text-2xl text-[#F7AB0A]">
+                                    Over 25 years specializing in commercial/industrial properties
+                                </p>
+                            </motion.div>
+                        </motion.div>
+
+                        <motion.div
+                            variants={textVariant(1.3)}
+                            className="max-[768px]:flex items-center justify-center relative z-20"
+                        >
+                            <a
+                                href="/listings"
+                                className="relative inline-block text-lg group mb-10 mt-10"
+                            >
+                                <span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white ">
+                                    <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
+                                    <span className="absolute left-0 w-64 h-64 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-[#F7AB0A] group-hover:-rotate-180 ease"></span>
+                                    <span className="relative">Browse Properties</span>
+                                </span>
+                                <span
+                                    className="absolute bottom-0 right-0 w-full h-12  -mb-1 -mr-1 transition-all duration-200 ease-linear bg-[#F7AB0A]  rounded-lg group-hover:mb-0 group-hover:mr-0"
+                                    data-rounded="rounded-lg"
+                                ></span>
+                            </a>
+                        </motion.div>
+                    </motion.div>
+                    <motion.div
+                        variants={fadeIn("left", "tween", 0.6, 0.95)}
+                        className="relative w-full"
+                    >
+                        <Image
+                            src="/bestPlace.svg"
+                            alt="heroImage"
+                            width={1000}
+                            height={1000}
+                            priority
+                            className="object-contain"
+                        />
+                    </motion.div>
+                </motion.div>
             </motion.div>
-          </motion.div>
-
-          <motion.div
-            variants={textVariant(1.3)}
-            className="max-[768px]:flex items-center justify-center relative z-20"
-          >
-            <a
-              href="/listings"
-              className="relative inline-block text-lg group mb-10 mt-10"
+            <motion.div
+                variants={staggerContainer}
+                initial="hidden"
+                whileInView="show"
+                viewport={{ once: false, amount: 0.25 }}
             >
-              <span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white ">
-                <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
-                <span className="absolute left-0 w-64 h-64 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-[#F7AB0A] group-hover:-rotate-180 ease"></span>
-                <span className="relative">Browse Properties</span>
-              </span>
-              <span
-                className="absolute bottom-0 right-0 w-full h-12  -mb-1 -mr-1 transition-all duration-200 ease-linear bg-[#F7AB0A]  rounded-lg group-hover:mb-0 group-hover:mr-0"
-                data-rounded="rounded-lg"
-              ></span>
-            </a>
-          </motion.div>
-        </motion.div>
-        <motion.div
-          variants={fadeIn("left", "tween", 0.6, 0.95)}
-          className="relative w-full"
-        >
-          <Image
-            src="/bestPlace.svg"
-            alt="heroImage"
-            width={1000}
-            height={1000}
-            priority
-              className="object-contain"
-          />
-       </motion.div> 
-      </motion.div>
-    </motion.div>
-           <motion.div
-      variants={staggerContainer}
-      initial="hidden"
-      whileInView="show"
-      viewport={{ once: false, amount: 0.25}}
-      >
 
-      <a href="#bulletsection" rel="norefferer" className="animate-pulse flex items-center justify-center mt-[64px] lg:mt-[86px] max-[425px]:mt-[82px] cursor-pointer">
-      <motion.img
-          variants={fadeIn('up', 'tween', 0.3,1)}
-          src="/arrow-down.svg"
-          alt="arrow down"
-          className="w-[38px] h-[38px] drop-shadow-xl hover:scale-105 transition-transform duration-200 ease-out object-contain  flex items-center justify-center rounded-full bg-[#F7AB0A] px-1 py-1"
-          >
-          </motion.img>
-      </a>
+                <a href="#bulletsection" rel="norefferer" className="animate-pulse flex items-center justify-center mt-[64px] lg:mt-[86px] max-[425px]:mt-[82px] cursor-pointer">
+                    <motion.img
+                        variants={fadeIn('up', 'tween', 0.3, 1)}
+                        src="/arrow-down.svg"
+                        alt="arrow down"
+                        className="w-[38px] h-[38px] drop-shadow-xl hover:scale-105 transition-transform duration-200 ease-out object-contain  flex items-center justify-center rounded-full bg-[#F7AB0A] px-1 py-1"
+                    >
+                    </motion.img>
+                </a>
 
-      </motion.div>  
-      </>
-  );
+            </motion.div>
+        </>
+    );
 };
 
 export default Hero;
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,154 +8,154 @@ import Image from 'next/image';
 import { HiX } from "react-icons/hi"
 
 const Navbar = () => {
-  const [toggle, setToggle] = useState(false)
-  const [navbar, setNavbar] = useState()
-  const [ rotate, setRotate ] = useState<boolean>(false)
+    const [toggle, setToggle] = useState(false)
+    const [navbar, setNavbar] = useState()
+    const [rotate, setRotate] = useState<boolean>(false)
 
-  // 1.solution for document being undefined, 2. useEffect, 3. import it like dynamic
-  useEffect(() => {
-    if ( typeof window !== "undefined" ) {
-      rotate ? document.body.style.overflow = "hidden" : document.body.style.overflow = "auto";
-    }
-  }, [rotate])
-
-
-  return (
-    <motion.nav
-      variants={navVariants}
-      initial="hidden"
-      whileInView="show"
-      viewport={{ once: true }}
-      className={`sm:px-16 px-3 py-2 overflow-x-hidden`}
-    >
-      <div className='w-full mx-auto flex items-center justify-between gap-8 py-5 left-0 top-0 '>
-        <Link href='/' className='cursor-pointer'>
-          <Image
-            src="/logoSvg.svg"
-            alt="logo"
-            width={150}
-            height={150}
-            priority
-          />
-        </Link>
-        <ul className='hidden lg:flex flex-1 relative items-center justify-center list-none space-x-6'>
-          {['home','listings', 'transactions', 'about'].map((item, index) => (
-            <div
-              key={item + index}
-            >
-              <li
-                className='mt-0 cursor-pointer flex flex-col items-center justify-center group list-none'
-                key={`${item}`}
-              >
-                <a
-                  href={`/${item}`}
-                  className={`${navbar
-                      ? "text-white group-hover:text-coffee-yellow"
-                      : "text-white  bg-left-bottom bg-gradient-to-r hover:text-[#F7AB0A] from-[#F7AB0A] to-[#F7AB0A] bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out"
-                    } uppercase no-underline font-bold`}
-                >
-                  {item}
-                </a>
-              </li>
-            </div>
-          ))}
-          <a href="/contact" className="relative inline-block text-lg group mb-10 mt-10">
-            <span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white ">
-              <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
-              <span className="absolute right-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-left rotate-90 translate-x-full translate-y-12 bg-[#F7AB0A]  group-hover:rotate-180 ease"></span>
-              <span className="relative">
-                Contact
-              </span>
-            </span>
-            <span
-              className="absolute bottom-0 right-0 w-full h-12  -mb-1 -mr-1 transition-all duration-200 ease-linear bg-[#F7AB0A]  rounded-lg group-hover:mb-0 group-hover:mr-0"
-              data-rounded="rounded-lg"
-            ></span>
-          </a>
-        </ul>
-        
+    // 1.solution for document being undefined, 2. useEffect, 3. import it like dynamic
+    useEffect(() => {
+        if (typeof window !== "undefined") {
+            rotate ? document.body.style.overflow = "hidden" : document.body.style.overflow = "auto";
+        }
+    }, [rotate])
 
 
-        <div
-          className="lg:hidden block relative"
-                    onClick={() => setRotate(prevState => !prevState)}
-        >
-          <div className="w-[30px] h-[30px] relative cursor-pointer"  onClick={() => setToggle(true)}>
-          <Image
-            src="/menu.svg"
-            alt="menu"
-            fill
-            className="object-contain"
-          />
-          </div>
-        </div>
-
-        {toggle && (
-          <motion.div
-            variants={mobileVariants}
+    return (
+        <motion.nav
+            variants={navVariants}
             initial="hidden"
             whileInView="show"
-            className="fixed z-[50] top-0 bottom-0 right-0 p-4 w-full
-             h-screen flex flex-col flex-shrink-0 items-end justify-end bg-[#121a34] bg-cover bg-repeat shadow-xl"
-          >
-            <div className="mt-[16px]" />
-            <div onClick={() => setRotate(prevState => !prevState )}>
-            <HiX
-              onClick={() => setToggle(false)}
-              className="w-[35px] h-[35px] text-white cursor-pointer ease-out transition-all duration-200"
-            />
-            </div>
-            <ul className="list-none p-0 m-0 h-full w-full flex items-center justify-start flex-col relative">
-              <div>
-                <a href="/" className="flex items-center justify-center">
-                  <Image
-                    src="/logoSvg.svg"
-                    alt="logo"
-                    width={150}
-                    height={150}
-                    priority
-                  />
-                </a>
-                <div className="mt-32">
-                {["home","listings", "transactions", "about"].map(
-                  (item, index) => (
-                    <div key={item + index} >
-                      <li className="m-4 group"
-                        key={`link-${item}`}
-                      >
-                        <a
-                          href={`/${item}`}
-                          className="text-white xl:text-2xl sm:text-xl
-                          uppercase bg-left-bottom bg-gradient-to-r hover:text-[#F7AB0A] from-[#F7AB0A] to-[#F7AB0A] bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out no-underline font-bold relative z-[50]"
-                          onClick={() => setToggle(false)}
+            viewport={{ once: true }}
+            className={`sm:px-16 px-3 py-2 overflow-x-hidden`}
+        >
+            <div className='w-full mx-auto flex items-center justify-between gap-8 py-5 left-0 top-0 '>
+                <Link href='/' className='cursor-pointer'>
+                    <Image
+                        src="/logoSvg.svg"
+                        alt="logo"
+                        width={250}
+                        height={250}
+                        priority
+                    />
+                </Link>
+                <ul className='hidden lg:flex flex-1 relative items-center justify-center list-none space-x-6'>
+                    {['home', 'listings', 'transactions', 'about'].map((item, index) => (
+                        <div
+                            key={item + index}
                         >
-                          {item}
-                        </a>
-                      </li>
+                            <li
+                                className='mt-0 cursor-pointer flex flex-col items-center justify-center group list-none'
+                                key={`${item}`}
+                            >
+                                <a
+                                    href={`/${item}`}
+                                    className={`${navbar
+                                        ? "text-white group-hover:text-coffee-yellow"
+                                        : "text-white  bg-left-bottom bg-gradient-to-r hover:text-[#F7AB0A] from-[#F7AB0A] to-[#F7AB0A] bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out"
+                                        } uppercase no-underline font-bold`}
+                                >
+                                    {item}
+                                </a>
+                            </li>
+                        </div>
+                    ))}
+                    <a href="/contact" className="relative inline-block text-lg group mb-10 mt-10">
+                        <span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white ">
+                            <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
+                            <span className="absolute right-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-left rotate-90 translate-x-full translate-y-12 bg-[#F7AB0A]  group-hover:rotate-180 ease"></span>
+                            <span className="relative">
+                                Contact
+                            </span>
+                        </span>
+                        <span
+                            className="absolute bottom-0 right-0 w-full h-12  -mb-1 -mr-1 transition-all duration-200 ease-linear bg-[#F7AB0A]  rounded-lg group-hover:mb-0 group-hover:mr-0"
+                            data-rounded="rounded-lg"
+                        ></span>
+                    </a>
+                </ul>
+
+
+
+                <div
+                    className="lg:hidden block relative"
+                    onClick={() => setRotate(prevState => !prevState)}
+                >
+                    <div className="w-[30px] h-[30px] relative cursor-pointer" onClick={() => setToggle(true)}>
+                        <Image
+                            src="/menu.svg"
+                            alt="menu"
+                            fill
+                            className="object-contain"
+                        />
                     </div>
-                  )
-                )}
                 </div>
-              </div>
-              <a href="/contact" className="relative inline-block text-lg group mr-8 mt-72">
-            <span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white ">
-              <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
-              <span className="absolute right-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-left rotate-90 translate-x-full translate-y-12 bg-[#F7AB0A]  group-hover:rotate-180 ease"></span>
-              <span className="relative">
-                Contact
-              </span>
-            </span>
-            <span
-              className="absolute bottom-0 right-0 w-full h-12  -mb-1 -mr-1 transition-all duration-200 ease-linear bg-[#F7AB0A]  rounded-lg group-hover:mb-0 group-hover:mr-0"
-              data-rounded="rounded-lg"
-            ></span>
-          </a>
-            </ul>
-          </motion.div>
-        )}
-      </div>
-    </motion.nav>
-  )
+
+                {toggle && (
+                    <motion.div
+                        variants={mobileVariants}
+                        initial="hidden"
+                        whileInView="show"
+                        className="fixed z-[50] top-0 bottom-0 right-0 p-4 w-full
+             h-screen flex flex-col flex-shrink-0 items-end justify-end bg-[#121a34] bg-cover bg-repeat shadow-xl"
+                    >
+                        <div className="mt-[16px]" />
+                        <div onClick={() => setRotate(prevState => !prevState)}>
+                            <HiX
+                                onClick={() => setToggle(false)}
+                                className="w-[35px] h-[35px] text-white cursor-pointer ease-out transition-all duration-200"
+                            />
+                        </div>
+                        <ul className="list-none p-0 m-0 h-full w-full flex items-center justify-start flex-col relative">
+                            <div>
+                                <a href="/" className="flex items-center justify-center">
+                                    <Image
+                                        src="/logoSvg.svg"
+                                        alt="logo"
+                                        width={150}
+                                        height={150}
+                                        priority
+                                    />
+                                </a>
+                                <div className="mt-32">
+                                    {["home", "listings", "transactions", "about"].map(
+                                        (item, index) => (
+                                            <div key={item + index} >
+                                                <li className="m-4 group"
+                                                    key={`link-${item}`}
+                                                >
+                                                    <a
+                                                        href={`/${item}`}
+                                                        className="text-white xl:text-2xl sm:text-xl
+                          uppercase bg-left-bottom bg-gradient-to-r hover:text-[#F7AB0A] from-[#F7AB0A] to-[#F7AB0A] bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out no-underline font-bold relative z-[50]"
+                                                        onClick={() => setToggle(false)}
+                                                    >
+                                                        {item}
+                                                    </a>
+                                                </li>
+                                            </div>
+                                        )
+                                    )}
+                                </div>
+                            </div>
+                            <a href="/contact" className="relative inline-block text-lg group mr-8 mt-72">
+                                <span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white ">
+                                    <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
+                                    <span className="absolute right-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-left rotate-90 translate-x-full translate-y-12 bg-[#F7AB0A]  group-hover:rotate-180 ease"></span>
+                                    <span className="relative">
+                                        Contact
+                                    </span>
+                                </span>
+                                <span
+                                    className="absolute bottom-0 right-0 w-full h-12  -mb-1 -mr-1 transition-all duration-200 ease-linear bg-[#F7AB0A]  rounded-lg group-hover:mb-0 group-hover:mr-0"
+                                    data-rounded="rounded-lg"
+                                ></span>
+                            </a>
+                        </ul>
+                    </motion.div>
+                )}
+            </div>
+        </motion.nav>
+    )
 }
 
 export default Navbar
