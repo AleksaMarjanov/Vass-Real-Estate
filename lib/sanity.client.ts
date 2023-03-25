4583f5fd6bd7a819a12d7a8fdebb6a0916f5561a
AleksaMarjanov <aleksa.marjanov@outlook.com>
2023-03-24 19:57:43 -0500

    env variables

--- lib/sanity.client.ts
+++ lib/sanity.client.ts
@@ -1,12 +1,9 @@
 import sanityClient from "@sanity/client";
 
 
-// export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
-// export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || "production";
-// const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION
-const projectId = '8m2psc99'
-const dataset = 'production'
-const apiVersion = '2021-11-15'
+export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
+export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || "production";
+const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION
 
 export const client = sanityClient({
     projectId,
