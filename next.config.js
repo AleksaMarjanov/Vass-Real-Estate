13fddfc5f7186c7b0e1bf24106f411f9c3e66e25
AleksaMarjanov <aleksa.marjanov@outlook.com>
2023-04-07 20:43:46 -0500

    update on MapParams and MapForContact

--- next.config.js
+++ next.config.js
@@ -16,7 +16,8 @@ const nextConfig = {
         dataset: process.env.NEXT_APP_PUBLIC_DATASET,
         emailJs_service: process.env.REACT_APP_EMAILJS_SERVICE,
         emailJs_template: process.env.REACT_APP_EMAILJS_TEMPLATE,
-        emailJs_API: process.env.REACT_APP_EMAILJS_API_KEY
+        emailJs_API: process.env.REACT_APP_EMAILJS_API_KEY,
+        apiKey: process.env.REACT_APP_LEAFLET_API_KEY
     },
     redirects: async () => {
         return [
