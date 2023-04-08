3523605d8d1e904a04e209062981f5b3921af645
AleksaMarjanov <aleksa.marjanov@outlook.com>
2023-04-07 20:54:44 -0500

    added back with protected env

---
+++ components/MapForContact.tsx
@@ -0,0 +1,36 @@
+
+"use client";
+
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
+import "leaflet-defaulticon-compatibility";
+
+const apiKey = process.env.apiKey
+
+const MapForContact = () => {
+
+
+    return (
+        <MapContainer
+            center={[48.148343, -103.62088]}
+            zoom={14}
+            scrollWheelZoom={false}
+            style={{ height: "100%", width: "100%" }}
+            attributionControl={false}
+        >
+            <TileLayer
+                url={`https://api.mapbox.com/styles/v1/mapbox/streets-v12/tiles/256/{z}/{x}/{y}@2x?access_token=${apiKey}`}
+            />
+            <Marker
+                position={[48.148343, -103.62088]}
+                // @ts-ignore
+                dragabble={true} >
+                <Popup>Our Office</Popup>
+
+            </Marker>
+        </MapContainer>
+    )
+}
+
+export default MapForContact
