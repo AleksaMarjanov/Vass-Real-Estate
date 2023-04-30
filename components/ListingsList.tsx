b7d05374fd307bfa47506adf3eb28c806a04146e
AleksaMarjanov <aleksa.marjanov@outlook.com>
2023-04-29 22:40:29 -0500

    ..

--- components/ListingsList.tsx
+++ components/ListingsList.tsx
@@ -25,68 +25,70 @@ const ListingsList = ({ listings }: Props) => {
             whileInView="show"
             viewport={{ once: true, amount: 0.25 }}
         >
-            <motion.div
-                variants={textVariant(1.1)}
-                // variants={slideIn('left', 'tween', 0.3, 1 )}
-                viewport={{ once: true }}
-                className='w-full relative z-25 text-bold text-2xl flex items-left justify-left px-12 max-[425px]:px-6 max-[425px]:py-3 py-6 mb-32 max-[425px]:mb-4'>
-                <h2>Available listings</h2>
-            </motion.div>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 px-10 gap-10 gap-y-16 pb-24 mt-6">
-                {listings.map((listing) => (
-                    <ClientSideRoute route={`/listing/${listing.slug.current}`} key={listing._id} >
-                        <motion.div
-                            variants={fadeIn("up", "tween", 0.5, 1)}
-                            initial="hidden"
-                            whileInView="show"
-                            viewport={{ once: true }}
-                            className="group flex flex-col cursor-pointer">
+            <>
+                <motion.div className="text-center"
+                    variants={fadeIn('up', 'tween', 0.5, 0.8)}
+                >
+                    <h2 className='font-bold mb-12 text-2xl '>
+                        Available listings
+                    </h2>
+                </motion.div>
+                <div className="mt-32 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 px-10 gap-10 gap-y-16 pb-24 mt-6">
+                    {listings.map((listing) => (
+                        <ClientSideRoute route={`/listing/${listing.slug.current}`} key={listing._id} >
+                            <motion.div
+                                variants={fadeIn("up", "tween", 0.5, 1)}
+                                initial="hidden"
+                                whileInView="show"
+                                viewport={{ once: true }}
+                                className="group flex flex-col cursor-pointer">
 
-                            <div className="relative w-full h-80 drop-shadow-xl group-hover:scale-105 transition-transform duration-200 ease-out">
-                                <Image
-                                    className="object-cover object-left lg:object-center rounded-lg"
-                                    src={urlFor(listing.mainImage).url()}
-                                    alt={listing.author.name}
-                                    fill
-                                    priority
-                                />
-                                <div className='absolute bottom-0 w-full bg-opacity-20 bg-black backdrop-blur-lg rounded drop-shadow-lg text-white p-5 flex justify-between'>
-                                    <div>
-                                        <p className='font-bold max-[768px]:text-[14px]'>{listing.title}</p>
-                                        <p className='max-[768px]:text-[12px]'>{new Date(listing._createdAt).toLocaleDateString("en-US", {
-                                            day: "numeric",
-                                            month: "long",
-                                            year: "numeric",
-                                        })}
-                                        </p>
-                                    </div>
+                                <div className="relative w-full h-80 drop-shadow-xl group-hover:scale-105 transition-transform duration-200 ease-out">
+                                    <Image
+                                        className="object-cover object-left lg:object-center rounded-lg"
+                                        src={urlFor(listing.mainImage).url()}
+                                        alt={listing.author.name}
+                                        fill
+                                        priority
+                                    />
+                                    <div className='absolute bottom-0 w-full bg-opacity-20 bg-black backdrop-blur-lg rounded drop-shadow-lg text-white p-5 flex justify-between'>
+                                        <div>
+                                            <p className='font-bold max-[768px]:text-[14px]'>{listing.title}</p>
+                                            <p className='max-[768px]:text-[12px]'>{new Date(listing._createdAt).toLocaleDateString("en-US", {
+                                                day: "numeric",
+                                                month: "long",
+                                                year: "numeric",
+                                            })}
+                                            </p>
+                                        </div>
 
-                                    <div className="flex flex-col md:flex-row gap-y-2 md:gap-x-2 items-center">
-                                        {listing.categories.map(category => (
-                                            <div key={category._id}>
-                                                <p className="font-bold max-[425px]:text-[12px]">{category.title}</p>
-                                            </div>
-                                        ))}
+                                        <div className="flex flex-col md:flex-row gap-y-2 md:gap-x-2 items-center">
+                                            {listing.categories.map(category => (
+                                                <div key={category._id}>
+                                                    <p className="font-bold max-[425px]:text-[12px]">{category.title}</p>
+                                                </div>
+                                            ))}
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
 
-                            <div className='mt-5 flex-1'>
-                                <p className='underline text-lg font-bold'>{listing.title}</p>
-                                <p className='text-whiteline-clamp-3'>
-                                    {listing.description}
+                                <div className='mt-5 flex-1'>
+                                    <p className='underline text-lg font-bold'>{listing.title}</p>
+                                    <p className='text-whiteline-clamp-3'>
+                                        {listing.description}
+                                    </p>
+                                </div>
+
+                                <p className="flex font-bold mt-5 items-center group-hover:underline">
+                                    Find Out More
+                                    <ArrowUpRightIcon className="ml-2 h-4 w-4" />
                                 </p>
-                            </div>
-
-                            <p className="flex font-bold mt-5 items-center group-hover:underline">
-                                Find Out More
-                                <ArrowUpRightIcon className="ml-2 h-4 w-4" />
-                            </p>
-                        </motion.div>
-                    </ClientSideRoute>
-                ))}
-            </div>
-        </motion.div>
+                            </motion.div>
+                        </ClientSideRoute>
+                    ))}
+                </div>
+            </>
+        </motion.div >
     )
 }
 
