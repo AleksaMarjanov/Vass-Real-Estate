41dfc2a30059c808bb3d42f7a022c30d28b21c85
Aleksa Marjanov <98630215+AleksaMarjanov@users.noreply.github.com>
2024-07-10 19:15:45 -0600

    Under maintenance

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,13 @@ import { Hero, Testimonials, FAQ, BulletSection, ServiceCard, TrustedBy, Relocat
 const HomePage = () => {
 
     return (
-
-        <main className="">
+        <main classname="h-full justify-center items-center">
+            <h1 classname="text-bold text-3xl">Website is under maintenance</h1>
+            </main>
+)
+        }
+export default HomePage;
+       /* <main className="">
             <section id="hero">
                 <Hero />
 
@@ -14,7 +19,7 @@ const HomePage = () => {
                 <BulletSection />
             </section>
             {/* Work around TypeScript Error Type 'Promise<Element>' is missing the following properties from type 'ReactElement<any, any>': type, props, key `ts(2786)`, workaround that is to use @ts-expect-error Server Component */}
-            <section id="trusted">
+         /*   <section id="trusted">
                 <TrustedBy />
             </section>
             <section id="services">
